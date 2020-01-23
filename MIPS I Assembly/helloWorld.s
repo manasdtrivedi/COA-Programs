3675accdc49b0053dcfaba45884a034646fac294
Manas Trivedi <manasdtrivedi@gmail.com>
2020-01-24 00:31:20 +0530

    Added 'helloWorld.s'
    'helloWorld.s' was added to the 'MIPS I Assembly' folder.

---
+++ MIPS I Assembly/helloWorld.s
@@ -0,0 +1,17 @@
+# MIPS-I assembly code to print "Hello World" on the console
+
+.data
+str:
+    .asciiz "Hello World"
+
+.text
+.globl main
+main:
+    addi $v0, $zero, 4
+    la $a0, str
+    syscall
+
+    li $v0, 10
+    syscall
+
+    .end
